7e1adfc977441f0da7aa48767dc791f5d27a048f
Dimitri Prima Nugraha <dimitriprimanugraha@gmail.com>
2022-09-01 14:35:04 +0700

    Newest

--- SIUUU/settings.py
+++ SIUUU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n*^k8=ltm3)wrs)$jsb8@4&f+6$mtp(-v_x=wz6_kma=fxkmj*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SIUUU.urls'
